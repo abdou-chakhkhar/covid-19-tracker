b6051a4311c1b0dc538c04288ed5c4ed982bf013
Abdou <abdelmoula.chakhkhar@uit.ac.ma>
2020-09-28 16:35:08 +0100

    map adn app

--- src/App.js
+++ src/App.js
@@ -12,9 +12,27 @@ import InfoBox from "./components/InfoBox/InfoBox";
 import LineGraph from "./components/LineGraph/LineGraph";
 import Map from "./components/Map/Map";
 
+import numeral from "numeral";
+import { prettyPrintStat } from "./util/util";
+
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState("worldwide");
+  const [country, setInputCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [casesType, setCasesType] = useState("cases");
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
+
+  console.log(countryInfo);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -26,11 +44,15 @@ function App() {
             value: country.countryInfo.iso2,
           }));
           setCountries(countries);
+          setMapCountries(data);
         });
     };
 
     getCountriesData();
   }, []);
+
+  console.log(mapCountries);
+  console.log(countries);
 
   const onCountryChange = async (e) => {
     const countryCode = e.target.value;
@@ -43,7 +65,10 @@ function App() {
     await fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        setCountry(countryCode);
+        setInputCountry(countryCode);
+        setCountryInfo(data);
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
       });
   };
 
@@ -67,11 +92,40 @@ function App() {
         </div>
 
         <div className="app__stats">
-          <InfoBox title="Coronavirus Cases" cases="+123k" total="123445" />
-          <InfoBox title="Recovered" cases="+123k" total="123445" />
-          <InfoBox title="Deaths" cases="+123k" total="123445" />
+          <InfoBox
+            onClick={(e) => setCasesType("cases")}
+            title="Coronavirus Cases"
+            isRed
+            active={casesType === "cases"}
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={numeral(countryInfo.cases).format("0.0a")}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType("recovered")}
+            title="Recovered"
+            active={casesType === "recovered"}
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={numeral(countryInfo.recovered).format("0.0a")}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType("deaths")}
+            title="Deaths"
+            isRed
+            active={casesType === "deaths"}
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={numeral(countryInfo.deaths).format("0.0a")}
+          />
+          <InfoBox
+            title="Active Cases"
+            cases={prettyPrintStat(countryInfo.active)}
+          />
         </div>
-        <Map />
+        <Map
+          countries={mapCountries}
+          casesType={casesType}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
       </div>
       <Card className="app__right">
         <CardContent>
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import "./Map.css";
+import { showDataOnMap } from "../../util/util";
 
-function Map() {
-  return <div>map goes here</div>;
+function Map({ countries, casesType, center, zoom }) {
+  console.log(countries);
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        >
+          {showDataOnMap(countries, casesType)}
+        </TileLayer>
+      </LeafletMap>
+    </div>
+  );
 }
 
 export default Map;
